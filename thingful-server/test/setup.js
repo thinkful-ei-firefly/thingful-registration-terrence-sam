a09436ad3d9507832d1ee7fa838d9f1c0092b68c
Sam Walker <jack.nasal@gmail.com>
2019-09-06 11:10:07 -0400

    initial commit

---
+++ thingful-server/test/setup.js
@@ -0,0 +1,12 @@
+'use strict';
+
+process.env.TZ = 'UTC';
+process.env.NODE_ENV = 'test';
+process.env.JWT_SECRET = 'test-jwt-secret';
+
+require('dotenv').config();
+const { expect } = require('chai');
+const supertest = require('supertest');
+
+global.expect = expect;
+global.supertest = supertest;
